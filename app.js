97038b35547c018673c98f2f696fbac1a29195cb
Natalia <nesleyne@gmail.com>
2018-04-09 00:33:19 +0200

    connected campgrounds to a database

--- app.js
+++ app.js
@@ -3,36 +3,29 @@ var app = express()
 var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/yelp_camp')
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
-var campgrounds = [
-  {
-    name: 'Salmon Creek',
-    image: 'https://farm4.staticflickr.com/3872/15119367505_f52d0d729e.jpg'
-  },
-  {
-    name: "cat's hiccup",
-    image: 'https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg'
-  },
-  {
-    name: 'fish fungal',
-    image: 'https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg'
-  }
-]
+mongoose.connect('mongodb://localhost/yelp_camp')
+
+var campSchema = new mongoose.Schema({
+  name: String,
+  image: String
+})
+var camp = mongoose.model('camp', campSchema)
 
 app.get('/', function (req, res) {
   res.render('landing')
 })
 
 app.get('/campgrounds', function (req, res) {
-  res.render('campgrounds', {
-    campgrounds: campgrounds
+  camp.find({}, function (err, campgrounds) {
+    if (err) {
+      console.log(err)
+    } else {
+      res.render('campgrounds', {campgrounds: campgrounds})
+    }
   })
 })
 
@@ -41,7 +34,13 @@ app.post('/campgrounds', function (req, res) {
     name: req.body.name,
     image: req.body.image
   }
-  campgrounds.push(newCamp)
+  camp.create(newCamp, function (err, camp) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(camp)
+    }
+  })
   res.redirect('/campgrounds')
 })
 
