aa79a1a2594128dd9decb1355583fedd896bea32
Marcin Zaborowski <jimrajnor@gmail.com>
2018-05-09 08:58:10 +0000

    add mongolab

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ app.use(express.static('public'))
 app.use(methodOverride('_method'))
 app.use(expressSanitizer('_method'))
 
-mongoose.connect('mongodb://localhost/yelp_camp')
+// mongoose.connect('mongodb://localhost/yelp_camp')
+mongoose.connect('mongodb://Thrajnor:7757@ds016098.mlab.com:16098/marcinyelpcamp')
 
 app.use(require("express-session")({
   secret: 'Natalia is again the best',
--- seeds.js
+++ seeds.js
@@ -7,37 +7,43 @@ var data = [{
     name: 'Salmon Creek',
     image: 'https://farm4.staticflickr.com/3872/15119367505_f52d0d729e.jpg',
     desc: 'description for Salmon Creek',
-    author: { username: 'Dummy Noone' }
+    author: { username: 'Dummy Noone' },
+    price: "3"
   },
   {
     name: "cat's hiccup",
     image: 'https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg',
     desc: "description for cat's hiccup",
-    author: { username: 'Dummy Noone' }
+    author: { username: 'Dummy Noone' },
+    price: "7"
   },
   {
     name: 'fish fungal',
     image: 'https://farm8.staticflickr.com/7258/7121861565_3f4957acb1.jpg',
     desc: 'description for fish fungal',
-    author: { username: 'Dummy Noone' }
+    author: { username: 'Dummy Noone' },
+    price: "5"
   },
   {
     name: 'Kitten lair',
     image: 'https://farm9.staticflickr.com/8579/16706717975_bdc99767d7.jpg',
     desc: 'description for Kitten lair',
-    author: { username: 'Dummy Noone' }
+    author: { username: 'Dummy Noone' },
+    price: '40'
   },
   {
     name: 'Koci Karton',
     image: 'https://farm7.staticflickr.com/6147/6040800665_f931d1ee3c.jpg',
     desc: 'description for Koci Karton',
-    author: { username: 'Dummy Noone' }
+    author: { username: 'Dummy Noone' },
+    price: '15.99'
   },
   {
     name: "Troll camp",
     image: "https://farm3.staticflickr.com/2924/14465824873_026aa469d7.jpg",
     desc: "An camp where you will be trolled",
-    author: { username: 'Dummy Noone' }
+    author: { username: 'Dummy Noone' },
+    price: '0'
   }
 ]
 
